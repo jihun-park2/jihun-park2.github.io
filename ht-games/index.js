90253d37b91e48c5be40409f59eb323c4b61041b
jihun-park2 <jihun-park2.github.io>
2019-04-30 20:04:46 +1000

    game-update

--- ht-games/index.js
+++ ht-games/index.js
@@ -1,10 +1,12 @@
 var cardDivs = document.querySelectorAll('.card');
 var levelOneContainer = document.querySelector('.level-1');
 var levelTwoContainer = document.querySelector('.level-2');
+var levelThreecontainer = document.querySelector('.level-3');
 
 
 var level = 1;
 var levelOneScore = 0;
+var levelTwoScore = 0;
 
 
 for(var i = 0; i < cardDivs.length; i++) {
@@ -37,14 +39,18 @@ function handlecardclick() {
 
             if (level == 1) {
                 levelOneScore++;
-            
+                levelTwoScore++;
 
             if (levelOneScore == 8) {
                 alert('level up')
                 levelOneContainer.style.display = "none";
                 levelTwoContainer.style.display = "grid";
             }
-
+            if (levelTwoScore == 18) {
+                alert('Gineus, level up')
+                levelTwoContainer.style.display = "none";
+                levelThreecontainer.style.display = "grid";
+            }
         }
         } else {
             setTimeout(flipover, 1500);
